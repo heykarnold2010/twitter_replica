54970adec1d7a6073ea5d591fc7db39c6447b258
heykarnold2010 <heykarnold@gmail.com>
2019-10-22 14:30:47 -0400

    Completed form pages, TODO: add posting, profile, database

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import Config
 
 
 app = Flask(__name__)
+
+# all variables based on this application has to be instantiated after the app instance is created
 bootstrap = Bootstrap(app)
 
 
+# flask app instance uses from object method to load in all configuartion variables
+app.config.from_object(Config)
+
+
 from app import routes
-
-
 # the way to run flask on a separate port
---
+++ app/forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, IntegerField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+class TitleForm(FlaskForm):
+    title = StringField('Enter a new header:', validators=[DataRequired()])
+    submit = SubmitField('ChangeTitle')
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name',
+    validators=[DataRequired()])
+    email = StringField('E-mail',
+    validators=[DataRequired(), Email()])
+    message = TextAreaField('Message',
+    validators=[DataRequired()])
+    submit = SubmitField('Send Message')
+
+
+
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name  = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    age = IntegerField('Age')
+    bio = StringField('Bio')
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Re-type Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
